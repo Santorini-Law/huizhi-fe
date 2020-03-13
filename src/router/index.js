8ffb2780617475a1ed6a38d2654d9b3df67acdb3
YinglishZhi <yinglishzhi@gmail>
2020-03-13 21:21:42 +0800

    照猫画虎

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import error404 from '@/views/common/error404'
+import HOME from '@/views/Home'
+
 Vue.use(Router)
 
 // eslint-disable-next-line no-unused-vars
@@ -12,6 +14,14 @@ function require (strings, resolve) {
 export default new Router({
   mode: 'history',
   routes: [
+    {
+      path: '/',
+      component: HOME,
+      meta: {
+        auth: true
+      },
+      name: 'Home'
+    }, // 首页
     {
       path: '/liuliuliu',
       name: 'HelloWorld',
